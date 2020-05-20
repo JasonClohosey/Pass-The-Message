a21fad4afa094929007d42ba60b72f348928706a
Jason Clohosey <clohoseyjason@gmail.com>
2020-05-19 22:08:48 -0400

    finished javascript minus input field clear

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-function deliverMeassage() {
+// function deliverMeassage() {
+//     document.getElementById("input").value;
+//     console.log("button Clicked")
+// };
+// deliverMeassage()
+// var message = ""
+
+document.getElementById("btn").addEventListener("click", function() {
+    var inputValue = document.getElementById("input").value;
+    console.log(inputValue);
     
+    document.getElementById("deliveredMessage").innerHTML = inputValue
 });
