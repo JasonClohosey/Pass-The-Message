22986494d69e4e88573c5494c08c8895a9a9f015
Jason Clohosey <clohoseyjason@gmail.com>
2020-05-21 11:15:24 -0400

    Added Input field clear feature. Finished css styling

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
-// function deliverMeassage() {
-//     document.getElementById("input").value;
-//     console.log("button Clicked")
-// };
-// deliverMeassage()
-// var message = ""
-
 document.getElementById("btn").addEventListener("click", function() {
     var inputValue = document.getElementById("input").value;
     console.log(inputValue);
     
     document.getElementById("deliveredMessage").innerHTML = inputValue
+
+    document.getElementById("input").value = ""
 });
